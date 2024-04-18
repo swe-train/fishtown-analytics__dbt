483a4e8deb9a6470ca56e9ccdd1fb3052aed16a4
Emily Rockman <emily.rockman@dbtlabs.com>
2024-04-18 12:04:59 -0500

    [BACKPORT 1.7] bump sqlparse (#9965)
    * bump sqlparse to 0.5 (#9951)
    
    * bump sqlparse
    
    * changelog
    # Conflicts:
    #	core/setup.py
    
    * fix change kind (#9964)
    
    * Update .changes/unreleased/Security-20240417-141316.yaml

--- core/setup.py
+++ core/setup.py
@@ -68,7 +68,7 @@ setup(
         "pathspec>=0.9,<0.12",
         "isodate>=0.6,<0.7",
         # ----
-        "sqlparse>=0.2.3,<0.5",
+        "sqlparse>=0.5.0,<0.6.0",
         # ----
         # These are major-version-0 packages also maintained by dbt-labs. Accept patches.
         "dbt-extractor~=0.5.0",
